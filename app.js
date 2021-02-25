93c83abde08b0e4090dfede3f94a6d159980d118
bradychen <bradychen2@gmail.com>
2021-02-25 13:20:57 +0800

    feature: add mongoose.

--- app.js
+++ app.js
@@ -1,9 +1,35 @@
 const express = require('express')
 const handlebars = require('express-handlebars')
 const bodyParser = require('body-parser')
+const mongoose = require('mongoose')
+const axios = require('axios').default
+const BASE_URL = "https://api.thecatapi.com"
+
+mongoose.connect('mongodb://localhost/kittipedia', {
+  useNewUrlParser: true, useUnifiedTopology: true
+})
+const db = mongoose.connection
 
 const app = express()
 const port = 3000
+
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
+axios.get(BASE_URL + '/v1/breeds')
+  .then((res) => {
+    breedsList = []
+    breedsList.push(...res.data)
+    console.log(breedsList)
+  })
+  .catch((err) => {
+    console.log(err)
+  })
 
 app.get('/', (req, res) => {
   res.send('app is running')
---
+++ models/cat.js
@@ -0,0 +1,60 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema
+
+const imageSchema = new Schema({
+  breeds: { type: breedSchema },
+  id: String,
+  url: String,
+  width: Number,
+  height: Number
+})
+
+const breedSchema = new Schema({
+  weight: {
+    imperial: String,
+    metric: String
+  },
+  id: String,
+  name: String,
+  cfa_url: String,
+  vetstreet_url: String,
+  vcahospitals_url: String,
+  temperament: String,
+  origin: String,
+  country_codes: String,
+  country_code: String,
+  description: String,
+  life_span: String,
+  indoor: { type: Number, max: 1, min: 0 },
+  lap: { type: Number, max: 1, min: 0 },
+  alt_names: String,
+  adaptability: { type: Number, max: 5, min: 1 },
+  affection_level: { type: Number, max: 5, min: 1 },
+  child_friendly: { type: Number, max: 5, min: 1 },
+  dog_friendly: { type: Number, max: 5, min: 1 },
+  energy_level: { type: Number, max: 5, min: 1 },
+  grooming: { type: Number, max: 5, min: 1 },
+  health_issues: { type: Number, max: 5, min: 1 },
+  intelligence: { type: Number, max: 5, min: 1 },
+  shedding_level: { type: Number, max: 5, min: 1 },
+  social_needs: { type: Number, max: 5, min: 1 },
+  stranger_friendly: { type: Number, max: 5, min: 1 },
+  vocalisation: { type: Number, max: 5, min: 1 },
+  experimental: { type: Number, max: 1, min: 0 },
+  hairless: { type: Number, max: 1, min: 0 },
+  natural: { type: Number, max: 1, min: 0 },
+  rare: { type: Number, max: 1, min: 0 },
+  rex: { type: Number, max: 1, min: 0 },
+  suppressed_tail: { type: Number, max: 1, min: 0 },
+  short_legs: { type: Number, max: 1, min: 0 },
+  wikipedia_url: String,
+  hypoallergenic: { type: Number, max: 1, min: 0 },
+  reference_image_id: String,
+  image: {
+    type: [imageSchema]
+  }
+})
+
+
+module.exports = mongoose.model('Breed', breedSchema)
+module.exports = mongoose.model('Image', imageSchema)

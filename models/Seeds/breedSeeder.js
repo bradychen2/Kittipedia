a90b0ad78f6eda7060e9968192f6ce652a7ab07b
bradychen <bradychen2@gmail.com>
2021-03-01 22:55:27 +0800

    modify seeders file

--- models/Seeds/breedSeeder.js
+++ models/Seeds/breedSeeder.js
@@ -1,18 +1,19 @@
 const Breed = require('../breed')
 const axios = require('axios').default
+const db = require('../../config/mongoose')
 const BASE_URL = "https://api.thecatapi.com"
 
-axios.get(BASE_URL + "/v1/breeds")
-  .then((res) => {
-    let breedsList = []
-    breedsList.push(...res.data)
-    breedsList.forEach(breed => {
-      Breed.create(breed)
+db.once('open', () => {
+  axios.get(BASE_URL + "/v1/breeds")
+    .then((res) => {
+      let breedsList = []
+      breedsList.push(...res.data)
+      breedsList.forEach(breed => {
+        Breed.create(breed)
+      })
+      console.log('breedSeeder done!')
     })
-    console.log('breedSeeder done!')
-  })
-  .catch((err) => {
-    console.log(err)
-  })
-
-
+    .catch((err) => {
+      console.log(err)
+    })
+})
--- models/Seeds/imageSeeder.js
+++ models/Seeds/imageSeeder.js
@@ -1,18 +1,19 @@
 const Image = require('../image')
 const axios = require('axios').default
+const db = require('../../config/mongoose')
 const BASE_URL = "https://api.thecatapi.com"
 
-axios.get(BASE_URL + "/v1/images/search?limit=90&page=10&order=Desc")
-  .then((res) => {
-    let gallery = []
-    gallery.push(...res.data)
-    gallery.forEach((image) => {
-      Image.create(image)
+db.once('open', () => {
+  axios.get(BASE_URL + "/v1/images/search?limit=90&page=10&order=Desc")
+    .then((res) => {
+      let gallery = []
+      gallery.push(...res.data)
+      gallery.forEach((image) => {
+        Image.create(image)
+      })
+      console.log('imageSeeder done!')
     })
-    console.log('imageSeeder done!')
-  })
-  .catch(err => {
-    console.log(err)
-  })
-
-
+    .catch(err => {
+      console.log(err)
+    })
+})
